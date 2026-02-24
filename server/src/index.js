3b059740da4c9277e1397e9bc977ac21d94ea9bb
Ganesh Singh Rawat <105rawawthimanshu@gmail.com>
2026-02-25 00:00:16 +0530

    refactor: reorganize route files and implement database connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRouter from "../routes/auth.routes.js";
-import messageRouter from "../routes/message.routes.js ";
+import authRouter from "./routes/auth.routes.js";
+import messageRouter from "./routes/message.routes.js ";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -12,4 +13,6 @@ const PORT=process.env.PORT||3000;
 app.use("/api/auth",authRouter);
 app.use("/api/messages", messageRouter);
 
-app.listen(PORT,()=>console.log(`server running on port ${PORT}`));
+app.listen(PORT,()=>{console.log(`server running on port ${PORT}`)
+connectDB();
+});
