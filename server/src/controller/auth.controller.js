eab2606e1520dd78a2aefd0bda1b1767988fe074
Ganesh Singh Rawat <105rawawthimanshu@gmail.com>
2026-02-27 21:56:47 +0530

    login-logout functionality added

--- server/src/controller/auth.controller.js
+++ server/src/controller/auth.controller.js
@@ -3,6 +3,7 @@ import bcrypt from "bcryptjs";
 import {generateToken} from "../lib/utils.js";
 import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 import {ENV} from "../lib/env.js";
+import { profile } from "console";
 
 export const signUp=async(req,res)=>{
   const {fullName,email,password}=req.body;
@@ -64,4 +65,36 @@ export const signUp=async(req,res)=>{
     res.status(500).json({message:"Server error"});
   }
 
+}
+
+
+export const login=async(req,res)=>{
+  const {email,password}=req.body;
+  try{
+    if(!email||!password){
+      return res.status(400).json({message:"All fields are required"});
+    }
+    const user=await User.findOne({email});
+    if(!user) return res.status(400).json({message:"Invalid credentials"});
+    const isPasswordCorrect=await bcrypt.compare(password,user.password);
+    if(!isPasswordCorrect) return res.status(400).json({message:"Invalid credentials"});
+
+    generateToken(user._id,res);
+    res.status(200).json({
+      message:"Login successful",
+     _id:user._id,
+     fullName:user.fullName,
+      email:user.email,
+      profilePicture:user.profilePicture
+    });
+  }
+  catch(error){
+    console.log("Error in login controller", error.message);
+    res.status(500).json({message:"Server error"});
+  }
+}
+
+export const logout=(_,res)=>{
+  res.cookie("jwt","",{maxAge:0})
+  res.status(200).json({message:"logged out succesfully"});
 }
