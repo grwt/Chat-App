9d32521435f6964c013c19a431038718e9959d45
Ganesh Singh Rawat <105rawawthimanshu@gmail.com>
2026-03-11 23:27:02 +0530

    Chat page layout setup

---
+++ client/src/store/useChatStore.js
@@ -0,0 +1,50 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
+import { useAuthStore } from "./useAuthStore";
+
+export const useChatStore= create(()=>({
+  allContacts:[],
+  chats:[],
+  messages:[],
+  activeTab:"chats",
+  selectedUser:null,
+  isUserLoading:false,
+  isMessageLoading:false,
+    isSoundEnabled: JSON.parse(localStorage.getItem("isSoundEnabled")) === true,
+
+  toggleSound:()=>{
+ localStorage.setItem("isSoundEnabled",!get().isSoundEnabled)
+ set({isSoundEnabled:!get().isSoundEnabled })
+  },
+
+  setActiveTab:(tab)=>set({activeTab:tab}),
+  setSelectedUser:(selectedUser)=>set({selectedUser}),
+
+  getAllContacts:async()=>{
+    set({isUserLoading:true});
+    try {
+      const res=await axiosInstance.get("/messages/contacts");
+      set({allContacts:res.data});
+    }catch(err){
+      toast.error(err.response.data.message);
+    }finally{
+      set({isUserLoading:false})
+    }
+  },
+
+
+  getMyChatPartners:async()=>{
+        set({isUserLoading:true});
+    try {
+      const res=await axiosInstance.get("/messages/chats");
+      set({chats:res.data});
+    }catch(err){
+      toast.error(err.response.data.message);
+    }finally{
+      set({isUserLoading:false})
+    }
+  },
+
+
+}))
